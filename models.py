a1eae3e38de09aabaa334282ded834589ad9c0ac
thijs-vanweezel <thijsvanweezel@gmail.com>
2024-10-01 11:51:45 +0200

    fix and test resnet

--- models.py
+++ models.py
@@ -20,30 +20,34 @@ class Forward(keras.Model):
             keras.layers.GroupNormalization(groups=-1),
             keras.layers.Activation("silu")    
         ])
+        self.conv1x1_1 = keras.layers.Conv2D(32, (1, 1), padding="same")
         self.block1 = block(32)
         self.pool1 = keras.layers.MaxPooling2D((2, 2))
+        self.conv1x1_2 = keras.layers.Conv2D(64, (1, 1), padding="same")
         self.block2 = block(64)
         self.pool2 = keras.layers.MaxPooling2D((2, 2))
+        self.conv1x1_3 = keras.layers.Conv2D(128, (1, 1), padding="same")
         self.block3 = block(128)
         self.pool3 = keras.layers.MaxPooling2D((2, 2))
         self.block4 = block(256)
 
     def call(self, x_t):
-        h_ = x_t
+        h_ = self.conv1x1_1(x_t)
         h = self.block1(x_t)
         h = keras.layers.add([h, h_]) # residual connection
-        h = h_ = self.pool1(h)
+        h = self.pool1(h)
 
+        h_ = self.conv1x1_2(h)
         h = self.block2(h)
         h = keras.layers.add([h, h_])
-        h = h_ = self.pool2(h)
+        h = self.pool2(h)
 
+        h_ = self.conv1x1_3(h)
         h = self.block3(h)
         h = keras.layers.add([h, h_])
-        h = h_ = self.pool3(h)
+        h = self.pool3(h)
 
-        h = self.block4(h)
-        h_t = keras.layers.add([h, h_])
+        h_t = self.block4(h)
         return h_t
 
 class Prior(keras.Model):
@@ -74,7 +78,8 @@ class Prior(keras.Model):
         return log_normal_diag(z, mu, log_var)
 
 class Posterior(keras.Model):
-    def __init__(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.net = keras.Sequential([
             keras.layers.Conv2D(512, (3, 3), padding="same"),
             keras.layers.Activation("silu"),
@@ -95,32 +100,38 @@ class Posterior(keras.Model):
 class Decoder(keras.Model):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        block = lambda filters, activation="silu": keras.Sequential([
+        block = lambda filters, activation="silu", strides=None: keras.Sequential([
             keras.layers.Dropout(.1),
             keras.layers.Conv2DTranspose(filters, (3, 3), padding="same"),
             keras.layers.Activation("silu"),
-            keras.layers.Conv2DTranspose(filters, (3, 3), strides=(2,2), padding="same"),
+            keras.layers.Conv2DTranspose(filters, (3, 3), strides=strides if strides else (2, 2), padding="same"),
             keras.layers.GroupNormalization(groups=-1),
             keras.layers.Activation(activation) 
         ])
+        self.conv1x1_1 = keras.layers.Conv2DTranspose(256, (1, 1), strides=(2, 2), padding="same")
         self.block1 = block(256)
+        self.conv1x1_2 = keras.layers.Conv2DTranspose(128, (1, 1), strides=(2, 2), padding="same")
         self.block2 = block(128)
+        self.conv1x1_3 = keras.layers.Conv2DTranspose(64, (1, 1), strides=(2, 2), padding="same")
         self.block3 = block(64)
-        self.block4 = block(6, "linear")
+        self.block4 = block(6, "linear", (1, 1))
 
     def call(self, z, h_t):
-        x = x_ = keras.layers.concatenate([z, h_t])
+        x = keras.layers.concatenate([z, h_t])
+
+        x_ = self.conv1x1_1(x)
         x = self.block1(x)
-        x = x_ = keras.layers.add([x, x_]) # residual connection
+        x = keras.layers.add([x, x_]) # residual connection
 
+        x_ = self.conv1x1_2(x)
         x = self.block2(x)
-        x = x_ = keras.layers.add([x, x_])
+        x = keras.layers.add([x, x_])
 
+        x_ = self.conv1x1_3(x)
         x = self.block3(x)
-        x = x_ = keras.layers.add([x, x_])
+        x = keras.layers.add([x, x_])
 
-        x = self.block4(x)
-        x_t_plus1_hat = keras.layers.add([x, x_])
+        x_t_plus1_hat = self.block4(x)
         return x_t_plus1_hat
 
     @staticmethod
@@ -132,33 +143,3 @@ class Decoder(keras.Model):
 
     def log_prob(self, x_t_plus1, x_t_plus1_hat):
         return self.log_bernoulli(x_t_plus1, keras.ops.sigmoid(x_t_plus1_hat))
-    
-# Instantiate models
-forward = Forward()
-prior = Prior()
-posterior = Posterior()
-decoder = Decoder()
-opt = keras.optimizers.AdamW()
-trainable_weights = forward.trainable_weights + prior.trainable_weights + posterior.trainable_weights + decoder.trainable_weights
-
-# The only part of the code that requires backend-specific ops
-def train_step(x_t, x_t_plus1):
-    # Forward pass
-    h_t = forward(x_t)
-    h_tplus1 = forward(x_t_plus1)
-    z, mu, log_var = posterior(h_t, h_tplus1)
-    kl_loss = posterior.log_prob(z, mu, log_var) - prior.log_prob(h_t, z)
-    rec_loss = decoder.log_prob(x_t_plus1, decoder(z, h_t))
-    loss = keras.ops.mean(-(rec_loss - kl_loss))
-
-    # Prepare backward pass
-    forward.zero_grad()
-    prior.zero_grad()
-    posterior.zero_grad()
-    decoder.zero_grad()
-
-    # Backward pass
-    loss.backward()
-    gradients = [t.value.grad for t in trainable_weights]
-    with torch.no_grad():
-        opt.apply_gradients(gradients, trainable_weights)
