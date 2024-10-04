bd5e8a878fbb13e03fa168afb363b821bac1bbc9
thijs-vanweezel <thijsvanweezel@gmail.com>
2024-10-04 11:25:26 +0200

    use separate forward for t_plus1

--- train.py
+++ train.py
@@ -1,34 +1,30 @@
 import os
 os.environ["KERAS_BACKEND"] = "torch"
 import keras, torch
-from models import Forward, Prior, Posterior, Decoder
-
-# Instantiate models
-forward = Forward()
-prior = Prior()
-posterior = Posterior()
-decoder = Decoder()
-opt = keras.optimizers.AdamW()
 
 # The only part of the code that requires backend-specific ops 
-def train_step(x_t, x_t_plus1):
+def train_step(x_t, x_t_plus1, forward_t, forward_tplus1, prior, posterior, decoder, opt):
     # Forward pass
-    h_t = forward(x_t)
-    h_tplus1 = forward(x_t_plus1)
+    h_t = forward_t(x_t)
+    h_tplus1 = forward_tplus1(x_t_plus1)
     z, mu, log_var = posterior(h_t, h_tplus1)
     kl_loss = posterior.log_prob(z, mu, log_var) - prior.log_prob(h_t, z)
     rec_loss = decoder.log_prob(x_t_plus1, decoder(z, h_t))
     loss = keras.ops.mean(-(rec_loss - kl_loss))
 
     # Prepare backward pass
-    forward.zero_grad()
+    forward_t.zero_grad()
+    forward_tplus1.zero_grad()
     prior.zero_grad()
     posterior.zero_grad()
     decoder.zero_grad()
 
     # Backward pass
     loss.backward()
-    trainable_weights = forward.trainable_weights + prior.trainable_weights + posterior.trainable_weights + decoder.trainable_weights
+    trainable_weights = forward_t.trainable_weights + forward_tplus1.trainable_weighst \
+          + prior.trainable_weights + posterior.trainable_weights + decoder.trainable_weights
     gradients = [t.value.grad for t in trainable_weights]
     with torch.no_grad():
         opt.apply_gradients(zip(gradients, trainable_weights))
+
+    return kl_loss, rec_loss
