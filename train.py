8827f4be45a686ca64366dcccd17220ff50cd15a
thijs-vanweezel <thijsvanweezel@gmail.com>
2024-10-22 16:22:03 +0200

    separate and simplify

--- train.py
+++ train.py
@@ -1,16 +1,20 @@
 import os
 os.environ["KERAS_BACKEND"] = "torch"
-import keras, torch
+import keras, torch, json
+from tqdm.auto import tqdm
 
-# The only part of the code that requires backend-specific ops 
+# The only function of the code that requires backend-specific ops 
 def train_step(x_t, x_t_plus1, forward_t, forward_tplus1, prior, posterior, decoder, opt):
+    # Move to gpu
+    x_t = x_t.to("cuda")
+    x_t_plus1 = x_t_plus1.to("cuda")
     # Forward pass
     h_t = forward_t(x_t)
     h_tplus1 = forward_tplus1(x_t_plus1)
     z, mu, log_var = posterior(h_t, h_tplus1)
-    kl_loss = keras.ops.sum(posterior.log_prob(z, mu, log_var) - prior.log_prob(h_t, z), axis=(1,2,3)) # Sum reduction
-    rec_loss = decoder.log_prob(x_t_plus1, decoder(z, h_t))
-    loss = keras.ops.mean(-(rec_loss - kl_loss))
+    kl_nll = keras.ops.sum(posterior.log_prob(z, mu, log_var) - prior.log_prob(h_t, z), axis=(1,2,3)) # Sum reduction
+    rec_ll = decoder.log_prob(x_t_plus1, decoder(z, h_t))
+    loss = keras.ops.mean(-rec_ll + kl_nll)
 
     # Prepare backward pass
     forward_t.zero_grad()
@@ -27,4 +31,29 @@ def train_step(x_t, x_t_plus1, forward_t, forward_tplus1, prior, posterior, deco
     with torch.no_grad():
         opt.apply_gradients(zip(gradients, trainable_weights))
 
-    return kl_loss, rec_loss
+    return keras.ops.mean(kl_nll).item(), keras.ops.mean(-rec_ll).item()
+
+def run(dataloader, forward_t, forward_tplus1, prior, posterior, decoder, optimizer, n_epochs=100):
+    # Loop over epochs
+    train_loss_history = {"kl_loss": [], "rec_loss": []}
+    os.makedirs("./results/basic0", exist_ok=True)
+    for i in tqdm(range(n_epochs)):
+        # Loop over batches
+        for j, (x_t, x_tplus1) in enumerate(dataloader, 1):
+            # Initialize scaler
+            if (i==0) and (j==1):
+                forward_t.layers[0].adapt(x_t)
+                forward_tplus1.layers[0].adapt(x_tplus1)
+            # Train
+            kl_loss, rec_loss = train_step(x_t, x_tplus1, forward_t, forward_tplus1, prior, posterior, decoder, optimizer)
+            train_loss_history["kl_loss"][i] = train_loss_history["kl_loss"][i]*(1-1/j) + 1/j*kl_loss
+            train_loss_history["rec_loss"][i] = train_loss_history["rec_loss"][i]*(1-1/j) + 1/j*rec_loss
+        # Save models 
+        forward_t.save("./results/basic0/forward_t.keras")
+        forward_tplus1.save("./results/basic0/forward_tplus1.keras")
+        prior.save("./results/basic0/prior.keras")
+        posterior.save("./results/basic0/posterior.keras")
+        decoder.save("./results/basic0/decoder.keras")
+        # Save training history
+        with open("./results/basic0/history.json", "w") as f:
+            json.dump(train_loss_history, f)
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
-import matplotlib.pyplot as plt
+import os
+os.environ["KERAS_BACKEND"] = "torch"
+import keras, json
+from matplotlib import pyplot as plt
 
 def plot_1d_statistic_over_time(data, statistic_idx, title):
     """
@@ -18,3 +21,45 @@ def plot_1d_statistic_over_time(data, statistic_idx, title):
     plt.xlabel("Time")
     plt.ylabel("Position on magnetic line")
     plt.show()
+
+def generate(true_trajectory, forward_t, prior, decoder):
+    # Generate trajectory by iteration
+    gen_trajectory = [true_trajectory[0]]
+    for i in range(len(true_trajectory)):
+        # Generate
+        h_t = forward_t(gen_trajectory[i])
+        z, *_ = prior(h_t)
+        gen_trajectory[i+1] = decoder(z, h_t)
+        # Concatenate forcing variables
+        gen_trajectory[i+1] = keras.ops.concatenate([
+            gen_trajectory[i+1], 
+            true_trajectory[i+1][...,-2:]
+        ], axis=-1)
+        # Save
+        gen_trajectory.append(gen_trajectory[i+1])
+    return gen_trajectory
+
+def plot_loss(filepath="./results/basic0/history.json"):
+    # Load training history
+    with open(filepath, "r") as f:
+        train_loss_history = json.load(f)
+    fig, ax1 = plt.subplots();
+    ax1.set_xlabel("Epoch");
+    ax1.set_ylabel("Küllback-Leibler divergence");
+    ax1.set_title("Training Loss");
+    ax1.plot(
+        list(map(lambda epoch: epoch["kl_loss"], train_loss_history)), 
+        color="black", 
+        label="Küllback-Leibler divergence",
+        linestyle="--"
+    );
+    plt.legend();
+    ax2 = ax1.twinx();
+    ax2.set_ylabel("Reconstruction loss");
+    ax2.plot(
+        list(map(lambda epoch: epoch["rec_loss"], train_loss_history)), 
+        color="black", 
+        label="Reconstruction loss",
+        linestyle=":"
+    );
+    plt.legend();
