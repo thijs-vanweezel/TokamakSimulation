b272be9cb0d9bcd96fc7cda52375695d25e75da8
famkevanree <famkevanree@hotmail.com>
2024-10-31 15:43:52 +0100

    PDE implementatie

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import os, math
 os.environ["KERAS_BACKEND"] = "torch"
 import keras, torch, json
 from tqdm.auto import tqdm
+import tensorflow as tf 
 
 def log_normal_diag(x, mu, log_var):
     log_p = -0.5 * keras.ops.log(2. * math.pi) - 0.5 * log_var - 0.5 * keras.ops.exp(-log_var) * (x - mu)**2.
@@ -13,8 +14,50 @@ def log_bernoulli(x, p):
     log_p = x * keras.ops.log(pp) + (1. - x) * keras.ops.log(1. - pp)
     return keras.ops.sum(log_p, [1,2,3]) # sum reduction
 
+def pde_loss(pred, true, B, x):
+    """
+    PDE loss function compatible with TensorFlow.
+    
+    Args:
+        pred: Prediction of the model x_tplus1_hat
+        true: True x_tplus1
+        B: Array of b-values (500 constant over instances and time)
+        x: Tensor, spatial position variable
+        t: Tensor, temporal position variable
+    
+    Returns:
+        f_n: Tensor, computed custom loss term
+    """
+    pred_n = pred[:, 0]
+    pred_v_parallel = pred[:, 1]
+    true_n = true[:, 0]
+    true_v_parallel = true[:, 1]
+    # ## Deel 1 van de formule vgm hoeft dit dus niet anders moeten we t hebben
+    # with tf.GradientTape() as tape_t:
+    #     tape_t.watch(t)
+    #     d_pred_n_dt = tape_t.gradient(pred_n, t)
+    #     d_true_n_dt = tape_t.gradient(true_n, t)
+
+    # temporal_loss = tf.square(d_pred_n_dt - d_true_n_dt)
+
+    ## Deel 2 ? In de wor 
+    pred_n_v_parallel = pred_n * pred_v_parallel / B
+    true_n_v_parallel = true_n * true_v_parallel / B
+
+    with tf.GradientTape() as tape_x:
+        tape_x.watch(x)
+        d_pred_nv_dx = tape_x.gradient(pred_n_v_parallel, x)
+        d_true_nv_dx = tape_x.gradient(true_n_v_parallel, x)
+
+    # Spatial loss component
+    spatial_loss = tf.square(B * (d_pred_nv_dx - d_true_nv_dx))
+
+    # Props to chat ligt er dus aan of we dat eerste deel willen 
+    # f_n = tf.reduce_mean(temporal_loss) + tf.reduce_mean(spatial_loss)
+    return spatial_loss
+
 # The only function of the code that requires backend-specific ops 
-def train_step(x_t, x_tplus1, forward_t, forward_tplus1, prior, posterior, decoder, opt):
+def train_step(x_t, x_tplus1, forward_t, forward_tplus1, prior, posterior, decoder, opt, x_tensor, b_field):
     # Move to gpu
     x_t = x_t.to("cuda")
     x_tplus1 = x_tplus1.to("cuda")
@@ -29,8 +72,10 @@ def train_step(x_t, x_tplus1, forward_t, forward_tplus1, prior, posterior, decod
         axis=[1,2,3]  # sum reduction
     )
     rec_ll = log_bernoulli(x_tplus1, x_tplus1_hat)
+    pde_loss_value = pde_loss(x_tplus1_hat, x_tplus1, b_field, x_tensor)
     loss = keras.ops.mean(-rec_ll + kl_nll) # mean reduction
-
+    loss += pde_loss_value
+  
     # Prepare backward pass
     forward_t.zero_grad()
     forward_tplus1.zero_grad()
@@ -60,7 +105,7 @@ def val_step(x_t, x_tplus1, forward_t, prior, decoder):
     # Return reconstruction loss
     return -log_bernoulli(x_tplus1, x_tplus1_hat)
 
-def run(train_loader, val_loader, forward_t, forward_tplus1, prior, posterior, decoder, optimizer, save_dir, max_epochs):
+def run(train_loader, val_loader, forward_t, forward_tplus1, prior, posterior, decoder, optimizer, b_field, x_tensor, save_dir, max_epochs):
     # Loop over epochs
     train_loss_history = {"kl_loss": [0]*max_epochs, "rec_loss": [0]*max_epochs}
     val_loss_hist = []
@@ -76,7 +121,7 @@ def run(train_loader, val_loader, forward_t, forward_tplus1, prior, posterior, d
                 x_t_hat = keras.ops.where(mask, x_t_hat.detach(), x_t[...,:-2]) # detach used in favor of retain_graph
                 x_t_hat = keras.ops.concatenate([x_t_hat, x_t[...,-2:]], axis=-1)
             # Train
-            x_t_hat, kl_loss, rec_loss = train_step(x_t_hat, x_tplus1, forward_t, forward_tplus1, prior, posterior, decoder, optimizer)
+            x_t_hat, kl_loss, rec_loss = train_step(x_t_hat, x_tplus1, forward_t, forward_tplus1, prior, posterior, decoder, optimizer, x_tensor, b_field)
             # Just a fancy way to append the mean
             train_loss_history["kl_loss"][i] = train_loss_history["kl_loss"][i]*(1-1/j) + 1/j*kl_loss
             train_loss_history["rec_loss"][i] = train_loss_history["rec_loss"][i]*(1-1/j) + 1/j*rec_loss
@@ -84,8 +129,8 @@ def run(train_loader, val_loader, forward_t, forward_tplus1, prior, posterior, d
         val_loss = 0
         for k, (x_t, x_tplus1) in enumerate(val_loader, 1):
             val_loss += val_step(x_t, x_tplus1, forward_t, prior, decoder)
-        val_loss_hist.append(val_loss/k)
-        if (i!=0) and (val_loss>val_loss_hist[i-1]):
+            val_loss_hist.append(val_loss/k)
+        if len(val_loss_hist) > 1 and val_loss > val_loss_hist[-2]:  # Compare with the previous epoch’s validation loss
             break
         # Save models 
         forward_t.save(f"{save_dir}/forward_t.keras")
@@ -94,5 +139,10 @@ def run(train_loader, val_loader, forward_t, forward_tplus1, prior, posterior, d
         posterior.save(f"{save_dir}/posterior.keras")
         decoder.save(f"{save_dir}/decoder.keras")
         # Save training history (trimmed to exclude default zeros)
+
+        trimmed_train_loss_history = {key: [val for val in values if val != 0] for key, values in train_loss_history.items()}
+        # Save the trimmed history to a JSON file
         with open(f"{save_dir}/history.json", "w") as f:
-            json.dump(train_loss_history[:train_loss_history.index(0)], f)
+            json.dump(trimmed_train_loss_history, f)
+        # with open(f"{save_dir}/history.json", "w") as f:
+        #     json.dump(train_loss_history[:train_loss_history.index(0)], f)
