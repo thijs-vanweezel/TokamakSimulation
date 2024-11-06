79e259c7e3fd3b592dbc230c93d330754226ad1e
thijs-vanweezel <thijsvanweezel@gmail.com>
2024-11-06 12:58:13 +0100

    do not print shape

--- utils.py
+++ utils.py
@@ -81,8 +81,6 @@ def plot_1d_statistic_over_time(data, statistic_idx, title):
         statistic_idx: int, index of the statistic to plot
         title: str, title of the plot
     """
-    print(data.shape)
-    print(data[:, :, statistic_idx].shape)
     fig = plt.figure(figsize=(12, 6))
     plt.imshow(data[:, :, statistic_idx].T, aspect="auto", cmap="viridis")
     plt.colorbar()
